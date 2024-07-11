4e6ed6afb4420e500f677955da68d498bddb1570
StewartBaker <bakrstewart16@gmail.com>
2024-07-11 10:59:26 -0500

    finished exercise

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,30 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your favorite School Subject?");
+            var favSubject = Console.ReadLine();
+
+            switch (favSubject.ToLower())
+            {
+                case "math":
+                    Console.WriteLine("Math was always one of my best subjects!");
+                    break;
+                case "science":
+                    Console.WriteLine("Science was a lot of fun, especially physics!");
+                    break;
+                case "english":
+                    Console.WriteLine("English was fun!");
+                    break;
+                case "social studies":
+                    Console.WriteLine("The class I fell asleep in the most");
+                    break;
+                case "art":
+                    Console.WriteLine("Let that creative side flow!");
+                    break;
+                default:
+                    Console.WriteLine($"Oh, I must have forgot about {favSubject}!");
+                    break;
+            }
         }
     }
 }
